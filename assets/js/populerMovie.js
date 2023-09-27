641b6a0ed488c412d867e720c3b2ff44c1e7efe3
yucelkaptan <127623528+yucelkaptan@users.noreply.github.com>
2023-09-27 12:21:56 +0300

    v5

--- assets/js/populerMovie.js
+++ assets/js/populerMovie.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
             var posterPath = movie.poster_path;
             if (posterPath) { 
                 var posterUrl = 'https://image.tmdb.org/t/p/w500' + posterPath;
-                var movieDiv = $('<div>').append('<img src="' + posterUrl + '" alt="Movie Poster" />');
+                var movieDiv = $('<div class="movie-div">').append('<img src="' + posterUrl + '" alt="Movie Poster" />');
+                var numberDiv = $('<div class="number-div">' + (i + 1) + '</div>'); // Sıra numarası ekleniyor
+                movieDiv.append(numberDiv); // Sıra numarasını movieDiv'e ekliyoruz
                 movieDiv.attr('data-id', movie.id); 
                 movieDiv.click(function() { 
                     var movieId = $(this).attr('data-id'); 
